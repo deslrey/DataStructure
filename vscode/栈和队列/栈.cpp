f50ec033a50292b388e9a6119d3b429f1dc309e9
deslre <2135232170@qq.com>
2024-10-10 10:35:07 +0800

    feat: 入栈操作

--- vscode/栈和队列/栈.cpp
+++ vscode/栈和队列/栈.cpp
@@ -17,6 +17,20 @@ void InitStack(SqStack &s)
     s.index = -1;
 }
 
+// 入栈操作
+bool Push(SqStack &s, DateType e)
+{
+    if (s.index == MAXSIZE - 1)
+    {
+        printf("当前栈以满,插入失败\n");
+        return false;
+    }
+    s.index++;
+    s.data[s.index] = e;
+    printf("插入成功\n");
+    return true;
+}
+
 // 判断是否为空栈
 bool EmptyStack(SqStack s)
 {
